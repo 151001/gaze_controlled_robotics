d7b531c7baa9578586d8b94ce84833430c37c0f4
151001 <151001@users.noreply.github.com>
2017-06-02 19:59:51 +0200

    Add files via upload

---
+++ Control/User/MainWindow.xaml.cs
@@ -0,0 +1,508 @@
+﻿using ANYCHATAPI;
+using EyeTribe.ClientSdk;
+using EyeTribe.ClientSdk.Data;
+using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Interop;
+using System.Windows.Media.Imaging;
+
+namespace User
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        #region Variables
+
+        // Server information
+        public string address = "demo.anychat.cn";
+        public int port = 8906;
+        public int roomNum = 151001;
+
+        // User information
+        public string userName = "s151001-student";
+        public string userPassword = "s151001";
+        private int myUserID = -1;
+
+        // Remote camera configuration
+        static public PresentationSource source;
+
+        public int remoteCamIndex = 0;
+        static public double remoteCamLeftP = 0.007;
+        static public double remoteCamRightP = 0.328;
+        static public double remoteCamTopP = 0.654;
+        static public double remoteCamBottomP = 0.959;
+
+        public int remoteCamLeft = 0;
+        public int remoteCamRight = 0;
+        public int remoteCamTop = 0;
+        public int remoteCamBottom = 0;
+
+        #endregion
+
+        #region Constructor
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            Log.Init();
+        }
+
+        #endregion
+
+        #region Window events
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+            // Init web video
+            try
+            {
+                IntPtr windowHdl = new WindowInteropHelper(Application.Current.MainWindow).Handle;
+
+                SystemSetting.Text_OnReceive = new TextReceivedHandler(ReceivedCmd); // Text callback
+                WebVideo.Init(windowHdl);
+                ShowInfo("Video server has been initialised. ");
+
+                int ret = AnyChatCoreSDK.Connect(address, port);
+                ret = AnyChatCoreSDK.Login(userName, userPassword, 0);
+                HwndSource hwndSource = HwndSource.FromHwnd(windowHdl);
+                hwndSource.AddHook(new HwndSourceHook(WndProc));
+                ShowInfo("Connecting to video server... ");
+            }
+            catch (Exception ex)
+            {
+                Log.SetLog("Exception: " + ex.Message.ToString());
+                ShowInfo("Error: " + ex.Message.ToString());
+            }
+
+            // Fit camera stream to window size
+            try
+            {
+                source = PresentationSource.FromVisual(this);
+                if (source != null)
+                {
+                    remoteCamLeft = Convert.ToInt32(remoteCamLeftP * SystemParameters.WorkArea.Width * source.CompositionTarget.TransformToDevice.M11);
+                    remoteCamRight = Convert.ToInt32(remoteCamRightP * SystemParameters.WorkArea.Width * source.CompositionTarget.TransformToDevice.M11);
+                    remoteCamTop = Convert.ToInt32(remoteCamTopP * SystemParameters.WorkArea.Height * source.CompositionTarget.TransformToDevice.M22);
+                    remoteCamBottom = Convert.ToInt32(remoteCamBottomP * SystemParameters.WorkArea.Height * source.CompositionTarget.TransformToDevice.M22);
+                }
+                else
+                {
+                    remoteCamLeft = Convert.ToInt32(remoteCamLeftP * SystemParameters.WorkArea.Width);
+                    remoteCamRight = Convert.ToInt32(remoteCamRightP * SystemParameters.WorkArea.Width);
+                    remoteCamTop = Convert.ToInt32(remoteCamTopP * SystemParameters.WorkArea.Height);
+                    remoteCamBottom = Convert.ToInt32(remoteCamBottomP * SystemParameters.WorkArea.Height);
+                }
+            }
+            catch (Exception ex)
+            {
+                Log.SetLog("Exception: " + ex.Message.ToString());
+                ShowInfo("Error: " + ex.Message.ToString());
+            }
+
+            // Maximize the main window and bring to front
+            WindowState = WindowState.Maximized;
+            Activate();         
+        }
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            try
+            {
+                WebVideo.Close(roomNum);
+            }
+            catch (Exception ex)
+            {
+                Log.SetLog("Exception: " + ex.Message.ToString());
+            }
+        }
+
+        #endregion
+
+        #region WndProc
+
+        private IntPtr WndProc(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
+        {
+            switch (msg)
+            {
+                case AnyChatCoreSDK.WM_GV_CONNECT:
+                    /// Connect
+                    int succeed = wParam.ToInt32();
+                    if (succeed == 1)
+                    {
+                        ShowInfo("Connected to video server. ");
+                    }
+                    else
+                    {
+                        Log.SetLog("Error: Connect to video server failed. ");
+                        ShowInfo("Error: Connect to video server failed. ");
+                    }
+                    break;
+                case AnyChatCoreSDK.WM_GV_LOGINSYSTEM:
+                    /// Login system
+                    if (lParam.ToInt32() == 0)
+                    {
+                        ShowInfo("Success login video server. ");
+                        myUserID = wParam.ToInt32();
+                        AnyChatCoreSDK.EnterRoom(roomNum, "", 0);
+                    }
+                    else
+                    {
+                        Log.SetLog("Login video server failed, Error=" + lParam.ToString());
+                        ShowInfo("Login video server failed. ");
+                    }
+                    break;
+                case AnyChatCoreSDK.WM_GV_ENTERROOM:
+                    // Enter room
+
+                    break;
+                case AnyChatCoreSDK.WM_GV_ONLINEUSER:
+                    /// Get users list
+                    int cnt = 0;    // Number of online users
+                    AnyChatCoreSDK.GetOnlineUser(null, ref cnt);    // Get the number of online users
+                    int[] usersID = new int[cnt];   // Online users ID list
+                    AnyChatCoreSDK.GetOnlineUser(usersID, ref cnt); // Get the ID list of online users
+                    for (int idx = 0; idx < cnt; idx++)
+                    {
+                        if (usersID[idx] != myUserID)
+                        {
+                            SetRemoteCamPos(usersID[idx], true);
+                        }
+                    }
+                    break;
+                case AnyChatCoreSDK.WM_GV_USERATROOM:
+                    /// New user enter room
+                    int userID = wParam.ToInt32();
+                    int boEntered = lParam.ToInt32();
+                    if (boEntered == 0)
+                    {
+                        if (userID != myUserID)
+                        {
+                            SetRemoteCamPos(userID, false);
+                        }
+                    }
+                    else
+                    {
+                        if (userID != myUserID)
+                        {
+                            SetRemoteCamPos(userID, true);
+                        }
+                    }
+                    break;
+                case AnyChatCoreSDK.WM_GV_CAMERASTATE:
+                    // State of the camera
+                    break;
+                case AnyChatCoreSDK.WM_GV_LINKCLOSE:
+                    // Lose connection
+                    AnyChatCoreSDK.LeaveRoom(-1);
+                    int wpara = wParam.ToInt32();
+                    int lpara = lParam.ToInt32();
+                    ShowInfo("Lose video connection. ");
+                    break;
+            }
+            return IntPtr.Zero;
+        }
+
+        private void SetRemoteCamPos(int id, bool en)
+        {
+            IntPtr windowHdl = new WindowInteropHelper(Application.Current.MainWindow).Handle;
+
+            byte[] userNameByte = new byte[255];
+            int ret = AnyChatCoreSDK.GetUserName(id, ref userNameByte[0], 30);
+            string userName = ByteToString(userNameByte);
+
+            if (string.Equals(userName, "s151001-robot"))
+            {
+                WebVideo.ControlRemoteVideo(id, en, windowHdl, remoteCamLeft, remoteCamTop, remoteCamRight, remoteCamBottom, remoteCamIndex);
+            }
+        }
+
+        private string ByteToString(byte[] byteStr)
+        {
+            string retVal = "";
+            try
+            {
+                retVal = System.Text.Encoding.GetEncoding("GB18030").GetString(byteStr, 0, byteStr.Length);
+            }
+            catch (Exception exp)
+            {
+                Console.Write(exp.Message);
+            }
+            return retVal.TrimEnd('\0');
+        }
+
+        #endregion 
+
+        #region Command handle
+
+        static public void SendCmd(string cmd)
+        {
+            bool secret = false;
+            int userID = -1; // -1 means to all users
+            int ret = -1;
+            ret = AnyChatCoreSDK.SendTextMessage(userID, secret, cmd, cmd.Length);
+        }
+
+        private void ReceivedCmd(int fromUID, int toUID, string Text, bool isserect)
+        {
+            //
+        }
+
+        #endregion
+
+        #region Show information
+
+        public void ShowInfo(string text)
+        {
+            sysInfoTxt.AppendText(DateTime.Now.ToString("HH:mm:ss") + ": " + text + "\r\n");
+            sysInfoTxt.ScrollToEnd();
+        }
+
+        #endregion
+
+        #region User interaction
+
+        static public BitmapImage setSource(string url)
+        {
+            return new BitmapImage(new Uri(url, UriKind.Relative));
+        }
+
+
+        // Front left
+        private void lfImg_MouseEnter(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            lfImg.Source = setSource("images/abtnbg.png");
+        }
+
+        private void lfImg_MouseLeave(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            lfImg.Source = setSource("images/btnbg.png");
+        }
+
+        private void lfImg_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            SendCmd("DK1020602070");
+            ShowInfo("Send front left command");
+        }
+
+        private void lficon_MouseEnter(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            lfImg.Source = setSource("images/abtnbg.png");
+        }
+
+        private void lficon_MouseLeave(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            lfImg.Source = setSource("images/btnbg.png");
+        }
+
+        private void lficon_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            SendCmd("DK1020602070");
+            ShowInfo("Send front left command");
+        }
+
+        // Forward
+        private void fImg_MouseEnter(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            fImg.Source = setSource("images/abtnbg.png");
+        }
+
+        private void fImg_MouseLeave(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            fImg.Source = setSource("images/btnbg.png");
+        }
+
+        private void fImg_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            SendCmd("DK1020602060");
+            ShowInfo("Send forward command");
+        }
+
+        private void ficon_MouseEnter(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            fImg.Source = setSource("images/abtnbg.png");
+        }
+
+        private void ficon_MouseLeave(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            fImg.Source = setSource("images/btnbg.png");
+        }
+
+        private void ficon_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            SendCmd("DK1020602060");
+            ShowInfo("Send forward command");
+        }
+
+        // Front right
+        private void rfImg_MouseEnter(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            rfImg.Source = setSource("images/abtnbg.png");
+        }
+
+        private void rfImg_MouseLeave(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            rfImg.Source = setSource("images/btnbg.png");
+        }
+
+        private void rfImg_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            SendCmd("DK1020702060");
+            ShowInfo("Send front right command");
+        }
+
+        private void rficon_MouseEnter(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            rfImg.Source = setSource("images/abtnbg.png");
+        }
+
+        private void rficon_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            SendCmd("DK1020702060");
+            ShowInfo("Send front right command");
+        }
+
+        // Left
+        private void lImg_MouseEnter(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            lImg.Source = setSource("images/abtnbg.png");
+        }
+
+        private void lImg_MouseLeave(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            lImg.Source = setSource("images/btnbg.png");
+        }
+
+        private void lImg_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            SendCmd("DK1010002030");
+            ShowInfo("Send left turn command");
+        }
+
+        private void licon_MouseEnter(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            lImg.Source = setSource("images/abtnbg.png");
+        }
+
+        private void licon_MouseLeave(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            lImg.Source = setSource("images/btnbg.png");
+        }
+
+        private void licon_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            SendCmd("DK1010002030");
+            ShowInfo("Send left turn command");
+        }
+
+        // Stop
+        private void stopImg_MouseEnter(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            stopImg.Source = setSource("images/abtnbg.png");
+        }
+
+        private void stopImg_MouseLeave(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            stopImg.Source = setSource("images/btnbg.png");
+        }
+
+        private void stopImg_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            SendCmd("DK0010001000");
+            ShowInfo("Send stop command");
+        }
+
+        private void stopicon_MouseEnter(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            stopImg.Source = setSource("images/abtnbg.png");
+        }
+
+        private void stopicon_MouseLeave(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            stopImg.Source = setSource("images/btnbg.png");
+        }
+
+        private void stopicon_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            SendCmd("DK0010001000");
+            ShowInfo("Send stop command");
+        }
+
+        // Right
+        private void rImg_MouseEnter(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            rImg.Source = setSource("images/abtnbg.png");
+        }
+
+        private void rImg_MouseLeave(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            rImg.Source = setSource("images/btnbg.png");
+        }
+
+        private void rImg_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            SendCmd("DK1020301000");
+            ShowInfo("Send right turn command");
+        }
+
+        private void ricon_MouseEnter(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            rImg.Source = setSource("images/abtnbg.png");
+        }
+
+        private void rficon_MouseLeave(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            rfImg.Source = setSource("images/btnbg.png");
+        }
+
+        private void ricon_MouseLeave(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            rImg.Source = setSource("images/btnbg.png");
+        }
+
+        private void ricon_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            SendCmd("DK1020301000");
+            ShowInfo("Send right turn command");
+        }
+
+        // Backwards
+        private void bImg_MouseEnter(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            bImg.Source = setSource("images/abtnbg.png");
+        }
+
+        private void bImg_MouseLeave(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            bImg.Source = setSource("images/btnbg.png");
+        }
+
+        private void bImg_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            SendCmd("DK1000300030");
+            ShowInfo("Send backward command");
+        }
+
+        private void bicon_MouseEnter(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            bImg.Source = setSource("images/abtnbg.png");
+        }
+
+        private void bicon_MouseLeave(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            bImg.Source = setSource("images/btnbg.png");
+        }
+
+        private void bicon_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            SendCmd("DK1000300030");
+            ShowInfo("Send backward command");
+        }
+
+        #endregion
+    }
+}
---
+++ HW/HW.ino
@@ -0,0 +1,303 @@
+#define FWD_L_PWM 3
+#define BACK_L_PWM 4
+#define FWD_R_PWM 23
+#define BACK_R_PWM 22
+#define BRAKES 21
+#define Ultra_en 20
+#define trig_r 1
+#define echo_r 2
+#define trig_mr 5
+#define echo_mr 6
+#define trig_m 7
+#define echo_m 8
+#define trig_ml 9
+#define echo_ml 10
+#define trig_l 11
+#define echo_l 12
+
+void setup() {
+	
+	// open the serial port
+	Serial.begin(9600);
+ 
+	// Motion control
+	pinMode(FWD_L_PWM, OUTPUT);
+	pinMode(BACK_L_PWM, OUTPUT);
+	pinMode(FWD_R_PWM, OUTPUT);
+	pinMode(BACK_R_PWM, OUTPUT);
+	pinMode(BRAKES, OUTPUT);
+	
+	// Ulatrasound sensors
+	pinMode(Ultra_en, OUTPUT); 
+	pinMode(trig_l, OUTPUT);
+	pinMode(echo_l, INPUT);
+	pinMode(trig_ml, OUTPUT);
+	pinMode(echo_ml, INPUT);
+	pinMode(trig_m, OUTPUT);
+	pinMode(echo_m, INPUT);
+	pinMode(trig_mr, OUTPUT);
+	pinMode(echo_mr, INPUT);
+	pinMode(trig_r, OUTPUT);
+	pinMode(echo_r, INPUT);	
+	delay(1000);
+	
+	// Init all pins
+	analogWrite(FWD_L_PWM, 0);
+	analogWrite(BACK_L_PWM, 0);
+	analogWrite(FWD_R_PWM, 0);
+	analogWrite(BACK_R_PWM, 0);
+	digitalWrite(BRAKES, HIGH);
+	digitalWrite(Ultra_en, HIGH);
+	digitalWrite(trig_l, LOW);
+	digitalWrite(trig_ml, LOW);
+	digitalWrite(trig_m, LOW);
+	digitalWrite(trig_mr, LOW);
+	digitalWrite(trig_r, LOW);
+	delay(1000);
+}
+
+int len = 12;
+char cmd[12] = {'0', '0', '0', '0', '1', '0', '0', '0', '1', '0', '0', '0'};
+int leftSpeed = 0;
+int rightSpeed = 0;
+int distance_l = 0; // in centimeters
+int distance_ml = 0;
+int distance_m = 0;
+int distance_mr = 0;
+int distance_r = 0;
+String lstate="1";
+String rstate="1";
+
+void Left() {
+	analogWrite(FWD_L_PWM, 0);
+	analogWrite(BACK_L_PWM, 0);
+	analogWrite(FWD_R_PWM, 30);
+	analogWrite(BACK_R_PWM, 0);
+	digitalWrite(BRAKES, HIGH);
+	lstate="1";
+	rstate="2";
+	leftSpeed=0;
+	rightSpeed=30;
+}
+
+void Right() {
+	analogWrite(FWD_L_PWM, 30);
+	analogWrite(BACK_L_PWM, 0);
+	analogWrite(FWD_R_PWM, 0);
+	analogWrite(BACK_R_PWM, 0);
+	digitalWrite(BRAKES, HIGH);
+	lstate="2";
+	rstate="1";
+	leftSpeed=30;
+	rightSpeed=0;
+}
+
+void Stall() {
+	analogWrite(FWD_L_PWM, 0);
+	analogWrite(BACK_L_PWM, 0);
+	analogWrite(FWD_R_PWM, 0);
+	analogWrite(BACK_R_PWM, 0);
+	digitalWrite(BRAKES, LOW);
+	lstate="1";
+	rstate="1";
+	leftSpeed=0;
+	rightSpeed=0;
+}
+
+int charToInt(char ch)
+{
+	int tmp = 0;
+	tmp = tmp * 10 + (ch - 48);
+	return tmp;
+}
+
+String intTostring(int n)
+{
+	int n1 = n%10;
+	String s1=String(n1);
+	n=n/10;
+	int n2 = n%10;
+	String s2=String(n2);
+	int n3=n/10;
+	String s3=String(n3);
+	return s3+s2+s1;
+}
+
+// state -> 0: fwd, back, Stall; <0: clockwise/right turn; >0: clounterclockwise/left turn;
+bool Reactive(int r, int l)
+{
+	rstate = String(r);
+	lstate = String(l);
+	int state=r-l;
+	
+	digitalWrite(trig_l, LOW);  
+	delayMicroseconds(2); 
+	digitalWrite(trig_l, HIGH);
+	delayMicroseconds(10); 
+	digitalWrite(trig_l, LOW);  
+	int duration_l = pulseIn(echo_l, HIGH);
+  
+	digitalWrite(trig_ml, LOW);  
+	delayMicroseconds(2); 
+	digitalWrite(trig_ml, HIGH);
+	delayMicroseconds(10); 
+	digitalWrite(trig_ml, LOW);  
+	int duration_ml = pulseIn(echo_ml, HIGH);
+
+	digitalWrite(trig_m, LOW);  
+	delayMicroseconds(2); 
+	digitalWrite(trig_m, HIGH);
+	delayMicroseconds(10); 
+	digitalWrite(trig_m, LOW);  
+	int duration_m = pulseIn(echo_m, HIGH);
+
+	digitalWrite(trig_mr, LOW);  
+	delayMicroseconds(2); 
+	digitalWrite(trig_mr, HIGH);
+	delayMicroseconds(10); 
+	digitalWrite(trig_mr, LOW);  
+	int duration_mr = pulseIn(echo_mr, HIGH);
+
+	digitalWrite(trig_r, LOW);  
+	delayMicroseconds(2); 
+	digitalWrite(trig_r, HIGH);
+	delayMicroseconds(10); 
+	digitalWrite(trig_r, LOW);  
+	int duration_r = pulseIn(echo_r, HIGH);
+  
+	distance_l = duration_l/58; // in centimeters
+	distance_ml = duration_ml/58;
+	distance_m = duration_m/58;
+	distance_mr = duration_mr/58;
+	distance_r = duration_r/58;
+	
+	if ( (r==2) && (l==2) ) {
+		if ( (distance_m < 80) || ( (distance_ml < 80)&&(distance_mr < 80) ) ) {
+			Stall();
+			return false;
+		}
+		else {
+			if ( (distance_ml < 80) || (distance_l < 20) ) {
+				Right();
+				return false;
+			}
+			if (distance_mr < 80 || (distance_r < 20) ) {
+				Left();
+				return false;
+			}
+		}
+	}
+	// Clockwise/Right turn
+	if (state<0) {
+		if ( (distance_r < 20) || (distance_ml <0) )
+		{
+			Stall();
+			return false;
+		}
+	}
+	// Clounterclockwise/Left turn
+	if (state>0) {
+		if ( (distance_l < 20) || (distance_mr < 20) )
+		{
+			Stall();
+			return false;
+		}
+	}
+	return true;
+}
+
+void DataPrint() {
+	Serial.println("DATA"+lstate+intTostring(leftSpeed)+rstate+intTostring(rightSpeed)+intTostring(distance_l)+intTostring(distance_ml)+intTostring(distance_m)+intTostring(distance_mr)+intTostring(distance_r) );
+	delay(10);
+}
+
+void loop() {
+	if (Serial.available() > 0) {
+		Serial.readBytes(cmd, len);
+	}
+	
+	// Analyse command
+	if (cmd[0] == 'D' && cmd[1] == 'K') {
+
+		// set left wheel direction and speed
+		leftSpeed = charToInt(cmd[5]) * 100 + charToInt(cmd[6]) * 10 + charToInt(cmd[7]);
+		if (leftSpeed>255) {
+		  leftSpeed=0;
+		}
+	
+		// set right wheel direction and speed
+		rightSpeed = charToInt(cmd[9]) * 100 + charToInt(cmd[10]) * 10 + charToInt(cmd[11]);
+		if (rightSpeed>255) {
+		  rightSpeed=0;
+		}
+		
+		// whether the Brake is enabled
+		switch (cmd[3]) {
+			case '0':
+				digitalWrite(BRAKES, HIGH);
+				break;
+			default:
+				digitalWrite(BRAKES, LOW);
+				Stall();
+				break;
+		}
+		
+		switch (cmd[4]) {
+			// Backward
+			case '0':
+				analogWrite(FWD_L_PWM, 0);
+				analogWrite(BACK_L_PWM, leftSpeed);
+				break;
+			// Forward
+			case '2':			
+				analogWrite(FWD_L_PWM, leftSpeed);
+				analogWrite(BACK_L_PWM, 0);
+				break;   
+			// Stall
+			default:
+				analogWrite(FWD_L_PWM, 0);
+				analogWrite(BACK_L_PWM, 0);
+				break;
+		}
+
+		switch (cmd[8]) {
+			// Backward
+			case '0':
+				analogWrite(FWD_R_PWM, 0);
+				analogWrite(BACK_R_PWM, rightSpeed);
+				break;
+			// Forward
+			case '2':
+				analogWrite(FWD_R_PWM, rightSpeed);
+				analogWrite(BACK_R_PWM, 0);
+				break;
+			// Stall
+			default:
+				analogWrite(FWD_R_PWM, 0);
+				analogWrite(BACK_R_PWM, 0);
+				break;
+		}	
+		
+		// Reactive control
+		if (cmd[2] == '1') {
+			// Enable all ultrasound sensors
+			digitalWrite(Ultra_en, LOW);
+			while(!Reactive(charToInt(cmd[8]), charToInt(cmd[4]))) {
+				DataPrint();
+				if (Serial.available() > 0) {
+					break;
+				}
+			}
+		DataPrint();
+		}			
+		else {
+			digitalWrite(Ultra_en, HIGH);
+		}
+		
+	}
+	// Error command
+	else {
+		Serial.flush();
+		Stall();		
+	}
+}
