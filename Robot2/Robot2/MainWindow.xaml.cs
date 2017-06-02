ce54add35e2b5f7cf9828d626c336ae9c0e5906b
151001 <151001@users.noreply.github.com>
2017-06-02 19:55:25 +0200

    Add files via upload

---
+++ Robot2/Robot2/MainWindow.xaml.cs
@@ -0,0 +1,178 @@
+﻿using ANYCHATAPI;
+using System;
+using System.Windows;
+using System.Windows.Interop;
+
+namespace Robot2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        #region Variables
+
+        // Server information
+        public string address = "demo.anychat.cn";
+        public int port = 8906;
+        public int roomNum = 151001;
+
+        // User information
+        public string userName = "s151001-usbcam";
+        public string userPassword = "s151001";
+        private int myUserID = -1;
+
+        // Local camera configuration
+        public int localCamIndex = 2;
+        
+        public int localCamLeft = 20;
+        public int localCamRight = 755;
+        public int localCamTop = 40;
+        public int localCamBottom = 530;
+
+        static public PresentationSource source;
+
+        #endregion
+
+        #region Constructor
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            Log.Init();
+        }
+
+        #endregion
+
+        #region Window Events
+
+        // Load window
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+            // Init web video
+            try
+            {
+                IntPtr windowHdl = new WindowInteropHelper(Application.Current.MainWindow).Handle;
+
+                WebVideo.Init(windowHdl);
+                ShowInfo("Video server has been initialised. ");
+
+                int ret = AnyChatCoreSDK.Connect(address, port);
+                ret = AnyChatCoreSDK.Login(userName, userPassword, 0);
+                HwndSource hwndSource = HwndSource.FromHwnd(windowHdl);
+                hwndSource.AddHook(new HwndSourceHook(WndProc));
+                ShowInfo("Connecting to video server... ");
+            }
+            catch (Exception ex)
+            {
+                Log.SetLog("Exception: " + ex.Message.ToString());
+                ShowInfo("Error: " + ex.Message.ToString());
+            }
+        }
+
+        // Close window
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            try
+            {
+                WebVideo.Close(roomNum);
+            }
+            catch (Exception ex)
+            {
+                Log.SetLog("Exception: " + ex.Message.ToString());
+            }
+        }
+
+        #endregion
+
+        #region WndProc
+
+        protected virtual IntPtr WndProc(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
+        {
+            switch (msg)
+            {
+                case AnyChatCoreSDK.WM_GV_CONNECT:
+                    /// Connect
+                    int succeed = wParam.ToInt32();
+                    if (succeed == 1)
+                    {
+                        ShowInfo("Connected to video server. ");
+                    }
+                    else
+                    {
+                        Log.SetLog("Error: Connect to video server failed. ");
+                        ShowInfo("Error: Connect to video server failed. ");
+                    }
+                    break;
+                case AnyChatCoreSDK.WM_GV_LOGINSYSTEM:
+                    /// Login system
+                    if (lParam.ToInt32() == 0)
+                    {
+                        ShowInfo("Success login video server. ");
+                        myUserID = wParam.ToInt32();
+                        AnyChatCoreSDK.EnterRoom(roomNum, "", 0);
+                    }
+                    else
+                    {
+                        Log.SetLog("Login video server failed, Error=" + lParam.ToString());
+                        ShowInfo("Login video server failed. ");
+                    }
+                    break;
+                case AnyChatCoreSDK.WM_GV_ENTERROOM:
+                    // Enter room
+                    int lparam = lParam.ToInt32();
+                    if (lparam == 0)
+                    {
+                        int roomid = wParam.ToInt32();
+                        ShowInfo("Success entered video server room. ");
+                        roomNum = roomid;
+
+                        // Open local video camera on the interface
+                        WebVideo.OpenLocalVideo(WebVideo.GetLocalVideoDeivceName(), hwnd, localCamLeft, localCamTop, localCamRight, localCamBottom, localCamIndex);
+                    }
+                    else
+                    {
+                        Log.SetLog("Enter video server room failed，" + lparam.ToString());
+                        ShowInfo("Enter video server room failed. ");
+                    }
+                    break;
+                case AnyChatCoreSDK.WM_GV_ONLINEUSER:
+                    /// Get users list
+                    int cnt = 0;    // Number of online users
+                    AnyChatCoreSDK.GetOnlineUser(null, ref cnt);    // Get the number of online users
+                    int[] usersID = new int[cnt];   // Online users ID list
+                    AnyChatCoreSDK.GetOnlineUser(usersID, ref cnt); // Get the ID list of online users            
+                    break;
+                case AnyChatCoreSDK.WM_GV_USERATROOM:
+                    /// New user enter room
+                    int userID = wParam.ToInt32();
+                    int boEntered = lParam.ToInt32();
+                    break;
+                case AnyChatCoreSDK.WM_GV_CAMERASTATE:
+                    // State of the camera
+                    break;
+                case AnyChatCoreSDK.WM_GV_LINKCLOSE:
+                    // Lose connection
+                    AnyChatCoreSDK.LeaveRoom(-1);
+                    int wpara = wParam.ToInt32();
+                    int lpara = lParam.ToInt32();
+                    ShowInfo("Lose  video connection. ");
+                    break;
+            }
+            return IntPtr.Zero;
+        }
+
+        #endregion
+
+        #region Show information
+
+        public void ShowInfo(string text)
+        {
+            sysInfoTxt.AppendText(DateTime.Now.ToString() + ": " + text + "\r\n");
+            sysInfoTxt.ScrollToEnd();
+        }
+
+        #endregion
+    }
+}
---
+++ Robot2/Robot2/WebVideo.cs
@@ -0,0 +1,72 @@
+﻿using ANYCHATAPI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Robot2
+{
+    class WebVideo
+    {
+        static public void Init(IntPtr hdl)
+        {
+            // Init AnychatSDK
+            string path = AppDomain.CurrentDomain.BaseDirectory;
+            AnyChatCoreSDK.SetSDKOption(AnyChatCoreSDK.BRAC_SO_CORESDK_PATH, path, path.Length);
+            SystemSetting.Init(hdl);
+        }
+
+        static public void Close(int roomNum)
+        {
+            AnyChatCoreSDK.LeaveRoom(roomNum);
+            AnyChatCoreSDK.Logout();
+            AnyChatCoreSDK.Release();
+        }
+
+        #region Local camera
+
+        static public void OpenLocalVideo(string[] videoDeviceName, IntPtr hwnd, int left, int right, int top, int bottom, int index)
+        {
+            try
+            {
+                int videoDeviceNum = videoDeviceName.Length;
+
+                AnyChatCoreSDK.SetUserStreamInfo(-1, index, AnyChatCoreSDK.BRAC_SO_LOCALVIDEO_DEVICENAME, videoDeviceName[index], videoDeviceName[index].ToCharArray().Length);
+                AnyChatCoreSDK.SetVideoPosEx(-1, hwnd, left, right, top, bottom, index, 0);
+                AnyChatCoreSDK.UserCameraControlEx(-1, true, index, 0, string.Empty);
+                AnyChatCoreSDK.UserSpeakControlEx(-1, false, index, 0, string.Empty);
+            }
+            catch (Exception ex)
+            {
+                Log.SetLog("Local video open error: " + ex.Message.ToString());
+            }
+        }
+
+        static public string[] GetLocalVideoDeivceName()
+        {
+            string[] retVal = null;
+
+            int deviceNum = 0;
+            AnyChatCoreSDK.EnumVideoCapture(null, ref deviceNum);
+            IntPtr[] deviceList = new IntPtr[deviceNum];
+            retVal = new string[deviceNum];
+
+            AnyChatCoreSDK.EnumVideoCapture(deviceList, ref deviceNum);
+            for (int idx = 0; idx < deviceNum; idx++)
+            {
+                IntPtr intPtr = deviceList[idx];
+                int len = 100;
+                byte[] byteArray = new byte[len];
+                Marshal.Copy(intPtr, byteArray, 0, len);
+                string DeviceName = Encoding.Default.GetString(byteArray);
+                DeviceName = DeviceName.Substring(0, DeviceName.IndexOf('\0'));
+                retVal[idx] = DeviceName;
+            }
+            return retVal;
+        }
+
+        #endregion
+    }
+}
